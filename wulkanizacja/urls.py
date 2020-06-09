74301288fcc8330c3c4d6ce40e1149a86caa620b
WojciechMalarski <Malarskiwojciech@gmail.com>
2020-06-09 22:45:45 +0200

    pushing

--- wulkanizacja/urls.py
+++ wulkanizacja/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('opony/', include('wulkanizacjaweb.urls')),
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('rejestracja/', rejestracja, name="rejestracja")
+    #path('rejestracja/', rejestracja, name="rejestracja")
 
     #path('', views.index, name='index'),
 
